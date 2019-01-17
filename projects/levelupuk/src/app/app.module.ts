393029d3e38b47dd7f011668db85942decc07efd
Hung Hoang <hung.h.progdev@gmail.com>
2019-01-17 00:55:42 +0000

    Added countdown timer, still working out some kinks

--- projects/levelupuk/src/app/app.module.ts
+++ projects/levelupuk/src/app/app.module.ts
@@ -6,19 +6,21 @@ import { AppComponent } from './app.component';
 import { BackgroundComponent } from './background/background.component';
 import { LinkbarComponent } from "../linkbar/linkbar.component";
 import { CountdownComponent } from './countdown/countdown.component';
+import { CountdownModule } from "ngx-countdown";
 
 @NgModule({
-  declarations: [
-    AppComponent,
+    declarations: [
+        AppComponent,
         BackgroundComponent,
-    LinkbarComponent,
-    CountdownComponent
-  ],
-  imports: [
-      BrowserModule,
-      NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+        LinkbarComponent,
+        CountdownComponent        
+    ],
+    imports: [
+        BrowserModule,
+        NgbModule,
+        CountdownModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- projects/levelupuk/src/app/countdown/countdown.component.ts
+++ projects/levelupuk/src/app/countdown/countdown.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Config } from "ngx-countdown";
 
 @Component({
-  selector: 'app-countdown',
-  templateUrl: './countdown.component.html',
-  styleUrls: ['./countdown.component.css']
+    selector: 'app-countdown',
+    templateUrl: './countdown.component.html',
+    styleUrls: ['./countdown.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class CountdownComponent implements OnInit {
 
-  constructor() { }
+    config: Config = {
+        leftTime: (+(new Date(Date.UTC(2019, 2, 23))).getTime() - +(new Date(Date.UTC(2019, 1, 17))).getTime()) / 1000,
+        repaint: function () {        
+            const me: any = this;
+            let content: string;
 
-  ngOnInit() {
-  }
+            me.hands.forEach((hand: any) => {
+                if (hand.lastValue !== hand.value) {
+                    content = '';
+                    const cur = me.toDigitals(hand.value + 1, hand.bits).join(''),
+                        next = me.toDigitals(hand.value, hand.bits).join('');
+
+                    hand.node.innerHTML = `
+                <span class="count curr top">${cur}</span>
+                <span class="count next top">${next}</span>
+                <span class="count next bottom">${next}</span>
+                <span class="count curr bottom">${cur}</span>
+              `;
+                    hand.node.parentElement.className = 'time';
+                    setTimeout(() => {
+                        hand.node.parentElement.className = 'time flip';
+                    });
+                }
+            });
+        },
+    };
+    
+    constructor() { }
+
+    ngOnInit() {        
+    }
 
 }
